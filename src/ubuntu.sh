5a3250d5db84779ccf7ef3430f0d14e4d85dd316
Eric Schrock <Eric.Schrock@delphix.com>
2019-09-10 22:50:20 -0400

    Retry apt-get on network failure or timeout
    Fixes #13

--- src/ubuntu.sh
+++ src/ubuntu.sh
@@ -4,12 +4,34 @@
 #
 
 #
+# apt-get can hang on poor network connections. This will enforce a reasonble
+# timeout and retry the download if it takes too long.
+#
+function safe_apt_get() {
+    local retry=3 count=0
+
+    while true; do
+        if timeout 60 apt-get install --download-only -y --fix-missing $*; then
+            break
+        fi
+        if (( count++ == retry )); then
+            printf 'Download $* failed\n' >&2
+            exit 1
+        fi
+    done
+
+    apt-get install -y $*
+}
+
+#
 # Get ubuntu-specific modules. With Ubuntu, we can get the pre-built modules and
 # headers for a given kernel release, so that we don't need to actually build from source.
 #
 function get_kernel() {
     apt-get update
-    apt-get install -y linux-modules-$KERNEL_RELEASE linux-headers-$KERNEL_RELEASE linux-source-$KERNEL_VERSION
+    safe_apt_get linux-modules-$KERNEL_RELEASE
+    safe_apt_get linux-headers-$KERNEL_RELEASE
+    safe_apt_get linux-source-$KERNEL_VERSION
     cd /usr/src && tar -xjf linux-source-$KERNEL_VERSION.tar.bz2
 
     KERNEL_SRC=/usr/src/linux-source-$KERNEL_VERSION
